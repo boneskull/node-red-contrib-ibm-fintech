648c97679aaf171223d73ee0760ac3343dcebaec
Christopher Hiller <boneskull@boneskull.com>
2018-04-17 22:27:53 -0700

    begin to handle options to investment-portfolio-read (wip)
    Signed-off-by: Christopher Hiller <boneskull@boneskull.com>

--- src/investment-portfolio-read/investment-portfolio-read.js
+++ src/investment-portfolio-read/investment-portfolio-read.js
@@ -1,26 +1,70 @@
+/// <reference types="node-red" />
+
+const parseDate = (date, time) => {
+  if (date) {
+    if (time) {
+      return new Date(`${date}T${time}Z`);
+    }
+    return new Date(`${date}T00:00:00.000Z`);
+  }
+};
+
+/**
+ * Creates the {@link InvestmentPortfolioReadNode} Node
+ * @export
+ * @param {Red} RED - Node-RED API
+ */
 export default function(RED) {
+  /**
+   * A Node to read Portfolio information
+   * @class InvestmentPortfolioReadNode
+   */
   class InvestmentPortfolioReadNode {
+    /**
+     * Creates an instance of InvestmentPortfolioReadNode.
+     * @param {any} [config={}] - Node config; see .html file
+     * @memberof InvestmentPortfolioReadNode
+     */
     constructor(config = {}) {
       RED.nodes.createNode(this, config);
 
-      Object.assign(this, {
-        name: config.name,
+      this.config = {
         portfolioName: config.portfolioName,
-        selector: config.selector,
-        service: RED.nodes.getNode(config.service)
-      });
+        atDate: parseDate(config.date, config.time),
+        hasKey: config.hasKey,
+        hasAnyKey: config.hasAnyKey,
+        hasNoKey: config.hasNoKey,
+        hasKeyValue: {
+          [config.hasKeyValueKey]: config.hasKeyValueValue
+        },
+        sort: config.sort,
+        limit: config.limit,
+        selector: config.selector
+      };
+
+      this.service = RED.nodes.getNode(config.service);
+      this.trace(`Config: ${JSON.stringify(config)}`);
 
       this.on('input', async msg => {
         if (this.service) {
-          const portfolios = await this.service.read({
-            portfolioName: this.portfolioName,
-            selector: this.selector
-          });
-          this.send({...msg, payload: portfolios});
+          try {
+            const portfolios = await this.service.read(this.config);
+            this.debug(`Received portfolios: ${JSON.stringify(portfolios)}`);
+            this.send({
+              ...msg,
+              payload: portfolios.shift()
+            });
+          } catch (err) {
+            this.error(err, msg);
+          }
+        } else {
+          this.warn(
+            `Message received, but no service configured: ${JSON.stringify(
+              msg
+            )}`
+          );
         }
       });
-
-      console.log(this);
     }
   }
 
