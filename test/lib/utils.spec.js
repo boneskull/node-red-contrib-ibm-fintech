5b3e77457e090f6df42fbffe707a578e819344aa
Christopher Hiller <boneskull@boneskull.com>
2018-10-31 15:29:32 -0700

    add some more tests for an awful (but less awful) coverage %

--- test/lib/utils.spec.js
+++ test/lib/utils.spec.js
@@ -22,4 +22,51 @@ describe('utils', function() {
       expect(utils.inspect`foo: ${data}`, 'not to match', /^foo::/);
     });
   });
+
+  describe('normalizeString()', function() {
+    describe('when given non-string parameter', function() {
+      it('should return void', function() {
+        expect(utils.normalizeString(), 'to be undefined');
+      });
+    });
+    describe('when given an empty string parameter', function() {
+      it('should return void', function() {
+        expect(utils.normalizeString(''), 'to be undefined');
+      });
+    });
+    describe('when given a non-empty string', function() {
+      it('should trim leading/trailing whitespace', function() {
+        expect(utils.normalizeString(' foo '), 'to be', 'foo');
+      });
+    });
+  });
+
+  describe('normalizeArray()', function() {
+    it('should compact empty string values', function() {
+      expect(utils.normalizeArray(['foo', '']), 'to equal', ['foo']);
+    });
+
+    it('should compact null values', function() {
+      expect(utils.normalizeArray(['foo', null]), 'to equal', ['foo']);
+    });
+
+    it('should compact undefined values', function() {
+      expect(utils.normalizeArray(['foo', undefined]), 'to equal', ['foo']);
+    });
+
+    it('should compact non-strings', function() {
+      expect(utils.normalizeArray(['foo', {}]), 'to equal', ['foo']);
+    });
+
+    it('should trim non-empty strings therein', function() {
+      expect(utils.normalizeArray(['foo', ' bar ']), 'to equal', [
+        'foo',
+        'bar'
+      ]);
+    });
+
+    it('should return void if array would be empty', function() {
+      expect(utils.normalizeArray([]), 'to be undefined');
+    });
+  });
 });
